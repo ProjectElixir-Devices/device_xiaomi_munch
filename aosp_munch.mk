342db9b552a00da17240af62f79e7ed5cb30934c
Ar5646L <aryan.v.bhatt@gmail.com>
2022-12-02 21:25:23 +0000

    munch: Correct device inherit comment

--- aosp_munch.mk
+++ aosp_munch.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from alioth device
+# Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
 # Inherit some common Project-Elixir stuff.
