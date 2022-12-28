7b36cc9d314f1c87cc64004f956976635f44965a
Ar5646L <aryan.v.bhatt@gmail.com>
2022-12-28 16:18:18 +0530

    munch: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,34 +5,30 @@ rm -rf kernel/xiaomi/sm8250-common
 rm -rf vendor/xiaomi
 rm -rf packages/resources/devicesettings
 
-echo 'Cloning DT-Common tree [1/8]'
+echo 'Cloning DT-Common tree [1/7]'
 # Device Tree Common
 git clone --depth=1 https://github.com/ProjectElixir-Devices/device_xiaomi_sm8250-common.git -b thirteen device/xiaomi/sm8250-common
 
-echo 'Cloning Kernel tree [2/8]'
+echo 'Cloning Kernel tree [2/7]'
 # Kernel Tree
 git clone --depth=1 https://github.com/madmax7896/kernel_xiaomi_sm8250.git -b arrow-13.0 kernel/xiaomi/sm8250
 
-echo 'Cloning Vendor tree [3/8]'
+echo 'Cloning Vendor tree [3/7]'
 # Vendor Tree
 git clone --depth=1 https://gitlab.com/Ar5646L/vendor_xiaomi_munch.git -b 13 vendor/xiaomi/munch
 
-echo 'Cloning Vendor-Common tree [4/8]'
+echo 'Cloning Vendor-Common tree [4/7]'
 # Vendor-Common Tree
 git clone --depth=1 https://github.com/Ar5646L/vendor_xiaomi_sm8250-common.git -b master vendor/xiaomi/sm8250-common
 
-echo 'Cloning Firmware [5/8]'
+echo 'Cloning Firmware [5/7]'
 # Firmware
 git clone --depth=1 https://gitlab.com/Ar5646L/vendor_xiaomi_munch-firmware.git -b twelve vendor/xiaomi/munch-firmware
 
-echo 'Cloning GcamGo [6/8]'
+echo 'Cloning GcamGo [6/7]'
 # GcamGo
 git clone --depth=1 https://gitlab.com/Ar5646L/vendor_xiaomi_haydn-extras.git -b twelve vendor/xiaomi/haydn-extras
 
-echo 'Cloning Mi Cam [7/8]'
-# Mi Cam
-git clone --depth=1 https://gitlab.com/spkal01/android_vendor_xiaomi_munch-miuicamera.git -b pyro vendor/xiaomi/munch-miuicamera
-
-echo 'Cloning Device Settings [8/8]'
+echo 'Cloning Device Settings [7/7]'
 # Device Settings
 git clone --depth=1 https://github.com/Evolution-X/packages_resources_devicesettings.git -b tiramisu packages/resources/devicesettings
